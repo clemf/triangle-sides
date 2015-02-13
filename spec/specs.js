eb1493eb4292a6263377a2bf95003c14d244862d
Clem <clemfree@gmail.com>
2015-02-13 08:40:05 -0800

    Add test for isosceles

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe('triangleSides', function() {
 	it('returns equilateral when all sides are equal', function() {
 		expect(triangleSides(5,5,5)).to.equal('equilateral');
 	});
+	it('returns isosceles if two sides are equal', function() {
+		expect(triangleSides(5,5,4)).to.equal('isosceles');
+	});
 });
