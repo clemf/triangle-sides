c45a99f211fa6ae80a6abbdbe32f53312e8e5957
Clem <clemfree@gmail.com>
2015-02-13 08:46:07 -0800

    Add logic for scalene check

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 function triangleSides(side1, side2, side3) {
+	
 	if(side1 === side2 && side2 === side3) {
-		return 'equilateral'
-	} else if(side1 === side2 || side2 === side3 || side3 === side1) {
-		return 'isosceles'
+		return 'equilateral';
+	}
+	
+	else if(side1 === side2 || side2 === side3 || side3 === side1) {
+		return 'isosceles';
+	}
+
+	else if(side1 !== side2 && side2 !== side3 || side3 !== side1) {
+		return 'scalene';
 	}
 }
