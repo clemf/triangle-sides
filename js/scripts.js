aa8af7a1d69d3ed0081e7613a0bf1f2ae56b6fd3
Clem <clemfree@gmail.com>
2015-02-13 08:41:53 -0800

    Add logic for isosceles check

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 function triangleSides(side1, side2, side3) {
 	if(side1 === side2 && side2 === side3) {
 		return 'equilateral'
+	} else if(side1 == side2 || side2 == side3 || side3 == side1) {
+		return 'isosceles'
 	}
 }
