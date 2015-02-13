9c34d1858946b00bc142122ca9af3844d181197a
Clem <clemfree@gmail.com>
2015-02-13 08:50:11 -0800

    Add test for invalid input

--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,18 @@
 describe('triangleSides', function() {
+	
 	it('returns equilateral when all sides are equal', function() {
 		expect(triangleSides(5,5,5)).to.equal('equilateral');
 	});
+	
 	it('returns isosceles if two sides are equal', function() {
 		expect(triangleSides(5,5,4)).to.equal('isosceles');
 	});
+	
 	it('returns scalene when no sides are equal', function() {
 		expect(triangleSides(5,4,3)).to.equal('scalene');
+	});
+
+	it('returns invalid when one side is as long as two combined', function() {
+		expect(triangleSides(10,5,5)).to.equal('invalid');
 	});
 });
